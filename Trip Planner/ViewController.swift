c8fc676fdbd00c76c35f3b949b57f73331100d6a
Michelle <mwehrlevt@gmail.com>
2021-10-14 10:24:48 -0400

    Initial commit

---
+++ Trip Planner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trip Planner
+//
+//  Created by Michelle on 10/14/21.
+//  Copyright © 2021 mwehrle. All rights reserved.
+// Trip planner
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

05e6dfb52b04c83df8de73fe3d12375f13f1185f
Michelle <mwehrlevt@gmail.com>
2021-10-16 11:00:35 -0400

    Created the initial buttons for the home page

--- Trip Planner/ViewController.swift
+++ Trip Planner/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // where do you want to go? label
+    @IBOutlet var placesToGoLabel: UILabel!
+    @IBOutlet var burlingtonButton: UIButton!
+    //override func viewDidLoad() {
+        //super.viewDidLoad()
+        //burlingtonButton.frame.size = CGSize(width:100, height:50)
+        //burlingtonButton.setBackgroundImage(UIImage(named: "Burlington-Vermont.jpg"), for: .normal)
+        //burlingtonButton.layer.cornerRadius = burlingtonButton.frame.size.height/2
+        //burlingtonButton.clipsToBounds = true
+        //burlingtonButton.contentMode = UIView.ContentMode.center
+    //}
+    // Burlington button
+    @IBAction func burlingtonButtonAction(_ sender: UIButton) {
+        
     }
-
-
+    
+    // Boston button
+    @IBAction func bostonButtonAction(_ sender: UIButton) {
+        
+    }
+    
+    // Chicago button
+    @IBAction func chicagoButtonAction(_ sender: UIButton) {
+        
+    }
+    
+    // New York City button
+    @IBAction func newYorkCityButtonAction(_ sender: UIButton) {
+        
+    }
+    
+    // Orlando button
+    @IBAction func orlandoButtonAction(_ sender: UIButton) {
+        
+    }
 }
 

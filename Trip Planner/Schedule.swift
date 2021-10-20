6230c2dcf36933fdb2462f0a27f77f5ce5e0440d
Zach Kiefer <zkiefer@uvm.edu>
2021-10-20 14:32:38 -0400

    Added Files and set up temp event creation

---
+++ Trip Planner/Schedule.swift
@@ -0,0 +1,9 @@
+//
+//  Schedule.swift
+//  Trip Planner
+//
+//  Created by user201571 on 10/20/21.
+//  Copyright © 2021 mwehrle. All rights reserved.
+//
+
+import Foundation
---
+++ Trip Planner/ScheduleElement.swift
@@ -0,0 +1,9 @@
+//
+//  ScheduleElement.swift
+//  Trip Planner
+//
+//  Created by user201571 on 10/20/21.
+//  Copyright © 2021 mwehrle. All rights reserved.
+//
+
+import Foundation

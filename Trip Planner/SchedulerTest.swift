39cb236a0a480888cf71b18af0ef566f678fbe49
Michelle <mwehrlevt@gmail.com>
2021-11-18 10:44:33 -0500

    Worked on core data

---
+++ Trip Planner/SchedulerTest.swift
@@ -0,0 +1,25 @@
+//
+//  SchedulerTest.swift
+//  Trip Planner
+//
+//  Created by Michelle on 11/17/21.
+//  Copyright © 2021 mwehrle. All rights reserved.
+//
+
+import CoreData
+
+
+/*class SchedulerTest {
+    //private var activities: [ActivitiesChosen] = [ActivitiesChosen]()
+    let persistenceController: PersistenceController
+    
+    init() {
+        //let persistenceController: PersistenceController
+        var activities: [ActivitiesChosen] = [ActivitiesChosen]()
+        populateActivities()
+    }
+    // get all the activities in a dictionary
+    private func populateActivities() {
+        activities = persistenceController.getAllActivities()
+    }
+}*/

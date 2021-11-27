5fdb4b7c8c2345cf7c4be06c37fe62512ff95ab6
Michelle <mwehrlevt@gmail.com>
2021-11-27 18:47:11 -0500

    Added Boston button functionality

---
+++ Trip Planner/BostonViews/BostonModelActivities.swift
@@ -0,0 +1,34 @@
+//
+//  BostonModelActivities.swift
+//  Trip Planner
+//
+//  Created by Michelle on 11/27/21.
+//  Copyright © 2021 mwehrle. All rights reserved.
+//
+
+import Foundation
+
+var bostonActivities: [BostonActivity] = load("BostonActivities.json")
+
+// fetches the json data
+func loadBoston<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+        else {
+            fatalError("Couldn't find \(filename) in main bundle")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle: \(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self): \(error)")
+    }
+}
